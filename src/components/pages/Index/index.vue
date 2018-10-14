2ac6a2b2e971c729b539a396f9c325237cdf57f7
ease <easetreychen@gmail.com>
2018-10-14 11:06:36 +0800

    引入element ui tab

--- src/components/pages/Index/index.vue
+++ src/components/pages/Index/index.vue
@@ -1,6 +1,12 @@
 <template>
     <div>
         <carousel></carousel>
+        <el-tabs v-model="activeName" @tab-click="handleClick">
+            <el-tab-pane label="用户管理" name="first">用户管理</el-tab-pane>
+            <el-tab-pane label="配置管理" name="second">配置管理</el-tab-pane>
+            <el-tab-pane label="角色管理" name="third">角色管理</el-tab-pane>
+            <el-tab-pane label="定时任务补偿" name="fourth">定时任务补偿</el-tab-pane>
+        </el-tabs>
     </div>
 </template>
 
@@ -8,8 +14,18 @@
     import carousel from './carousel'
     export default {
         name: "index",
+        data(){
+            return{
+                activeName: 'first'
+            }
+        },
         components:{
             carousel
+        },
+        methods: {
+            handleClick(tab, event) {
+                console.log(tab, event);
+            }
         }
     }
 </script>
--- src/main.js
+++ src/main.js
@@ -2,16 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store/index'
-import svg from './helpers/svgfont'
+import svg from './helpers/svgfont'      //引入阿里巴巴iconfront-svg
+import ElementUI from 'element-ui';      //引入elementUI
+import 'element-ui/lib/theme-chalk/index.css';
+
 
 Vue.config.productionTip = false
+
+
+Vue.use(ElementUI);
 
 new Vue({
     router,
     store,
     svg,
-    render
-:
-h => h(App)
-}).
-$mount('#app')
+    render:
+        h => h(App)
+}).$mount('#app')
