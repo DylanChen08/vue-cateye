b59ee760bfdd55e705c4f1a818c714a7923279ec
ease <easetreychen@gmail.com>
2018-10-23 10:32:21 +0800

    删除输了首页的数据，改为后端提供

--- src/store/index.js
+++ src/store/index.js
@@ -4,14 +4,12 @@ Vue.use(Vuex)
 
 import getData from './modules/getData'
 import carousel from './modules/carousel'
-import pageIndex from './modules/pageIndex'
 import movieReleasedList from './modules/movieReleasedList'
 
 export default new Vuex.Store({
    modules:{
        getData,
        carousel,
-       pageIndex,
        movieReleasedList
    }
 })
